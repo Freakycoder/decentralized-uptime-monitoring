08e016c11f8a9284f8329279ef316b829d10f5b4
Ahmed <ahmxd989@gmail.com>
2025-04-15 19:44:26 +0400

    updated

---
+++ app/frontend/src/backend/src/entities/validator.rs
@@ -0,0 +1,21 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)] 
+#[sea_orm(table_name = "Validators")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: Uuid,
+    pub user_id: Uuid,
+    #[sea_orm(unique)]
+    pub wallet_address : String,
+    #[sea_orm(default_value = "CURRENT_TIMESTAMP")]
+    pub created_at : Option<DateTimeWithTimeZone>
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(belongs_to = "super::user::Entity" , from = "Column::UserId", to = "super::user::Column::Id")]
+    Users
+}
+
+impl ActiveModelBehavior for ActiveModel {}
