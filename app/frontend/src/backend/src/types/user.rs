525de317ad63235232e65f7cd90231ad001809a2
Ahmed <ahmxd989@gmail.com>
2025-04-19 17:53:24 +0400

    updated

--- app/frontend/src/backend/src/types/user.rs
+++ app/frontend/src/backend/src/types/user.rs
@@ -6,3 +6,17 @@ pub struct UserInput{
     pub email : String,
     pub password : String
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SignUpResponse{
+    pub status_code : u32,
+    pub message : String,
+    pub token : Option<String>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+   pub sub: String,           
+   pub exp: usize,           
+    
+}
