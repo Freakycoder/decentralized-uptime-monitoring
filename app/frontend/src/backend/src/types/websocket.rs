3e25f9666bb22c0654f72b3885793976a83bc954
Ahmed <ahmxd989@gmail.com>
2025-05-02 20:09:47 +0400

    updated

---
+++ app/frontend/src/backend/src/types/websocket.rs
@@ -0,0 +1,59 @@
+use sea_orm::prelude::DateTimeWithTimeZone;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ValidatorMessage {
+    #[serde(rename = "register_validator")]
+    RegisterValidator {
+        validator_id: String,
+        #[serde(default)]
+        location: Option<Location>,
+    },
+    #[serde(rename = "website_status")]
+    WebsiteStatus {
+        url: String,
+        status: WebsiteStatus,
+        response_time: u32,
+        timestamp: String,
+        #[serde(default)]
+        details: Option<StatusDetails>,
+        validator_id: String,
+        latitude: f64,
+        longitude: f64,
+    },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum WebsiteStatus {
+    Up,
+    Down,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct StatusDetails {
+    pub dns_lookup: Option<u32>,
+    pub tcp_connection: Option<u32>,
+    pub tls_handshake: Option<u32>,
+    pub ttfb: Option<u32>,
+    pub content_download: Option<u32>,
+    pub http_status_code: Option<u16>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ValidatorConnection {
+    pub validator_id: String,
+    pub location: Option<Location>,
+    pub connected_at: DateTimeWithTimeZone,
+    pub last_active: DateTimeWithTimeZone,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Location {
+    pub latitude: f64,
+    pub longitude: f64,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct ServerMessage {
+    url: String,
+}
