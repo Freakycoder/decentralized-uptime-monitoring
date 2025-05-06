97ca161099eba5086831cafe652f6276006a4297
Ahmed <ahmxd989@gmail.com>
2025-05-06 20:07:12 +0400

    updated

--- app/frontend/src/backend/src/entities/website_register.rs
+++ app/frontend/src/backend/src/entities/website_register.rs
@@ -6,19 +6,22 @@ pub struct Model{
     #[sea_orm(primary_key)]
     pub id: Uuid,
     #[sea_orm(unique)]
-    pub website: String,
+    pub website_url: String,
+    pub user_id : String,
+    #[sea_orm(default_value = "CURRENT_TIMESTAMP")]
     pub timestamp: DateTimeWithTimeZone,
-    pub is_reachable: bool,
-    pub http_status_code: Option<i32>,
-    pub error_type: Option<String>,
-    pub connection_type: Option<String>,
-    pub provider: Option<String>,
-    pub latitude: Option<f64>,
-    pub longitude: Option<f64>,
-    pub is_validated: bool,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(belongs_to = "super::user::Entity", from = "Column::UserId", to = "super::user::Column::Id")]
+    Users,
+}
 
 impl ActiveModelBehavior for ActiveModel {}
+
+impl Related<super::user::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Users.def()
+    }
+}
