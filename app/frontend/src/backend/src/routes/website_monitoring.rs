82b3093720cefb79651f384adecf332c37cd093f
Ahmed <ahmxd989@gmail.com>
2025-05-06 20:07:29 +0400

    updated

--- app/frontend/src/backend/src/routes/website_monitoring.rs
+++ app/frontend/src/backend/src/routes/website_monitoring.rs
@@ -1,13 +1,62 @@
+use crate::entities::website_register;
 use axum::{
- routing::get, Router
+    Json, Router,
+    extract::State,
+    routing:: post,
 };
+use sea_orm::{ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, Set};
 
+use crate::types::website::{AddWebsiteInput, AddWebsiteResponse};
 
-pub fn website_router() -> Router{
-    Router::new()
-    .route("/signup", get(signup))
+pub fn website_router() -> Router<DatabaseConnection> {
+    Router::new().route("/add-website", post(website_to_add))
 }
 
-async fn signup() -> &'static str{
-    "hello from website status"
+#[axum::debug_handler]
+async fn website_to_add(
+    State(db): State<DatabaseConnection>,
+    Json(website_data): Json<AddWebsiteInput>,
+) -> Json<AddWebsiteResponse> {
+    let url = website_data.url_to_monitor;
+    let existing_url = website_register::Entity::find()
+        .filter(website_register::Column::WebsiteUrl.eq(&url))
+        .one(&db)
+        .await;
+
+    if let Err(db_err) = existing_url {
+        return Json(AddWebsiteResponse {
+            status_code: 500,
+            message: format!("db error occured : {}", db_err),
+        });
+    }
+
+    if let Some(_) = existing_url.unwrap() {
+        return Json(AddWebsiteResponse {
+            status_code: 409,
+            message: format!("website already exist"),
+        });
+    }
+
+    let new_url = website_register::ActiveModel {
+        website_url: Set(url),
+        user_id: Set(String::from("ahmed")),
+        ..Default::default()
+    };
+
+    let result = new_url.insert(&db).await;
+
+    match result {
+        Ok(_) => {
+            return Json(AddWebsiteResponse {
+                status_code: 200,
+                message: format!("New URL registered succesfully"),
+            });
+        }
+        Err(db_err) => {
+            return Json(AddWebsiteResponse {
+                status_code: 404,
+                message: format!("Db error occured : {}", db_err),
+            });
+        }
+    }
 }
