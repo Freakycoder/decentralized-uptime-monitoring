5dd0da391eb5ae2f2e1ab029b9a360f1b1b90eeb
Ahmed <ahmxd989@gmail.com>
2025-05-11 13:16:27 +0400

    updated

--- app/frontend/src/backend/src/types/user.rs
+++ app/frontend/src/backend/src/types/user.rs
@@ -2,7 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserInput{
-    pub username : String,
     pub email : String,
     pub password : String
 }
