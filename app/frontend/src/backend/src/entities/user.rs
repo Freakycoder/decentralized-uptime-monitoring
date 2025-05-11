4526dfe0a6b2195e8b69ff45f54c4fd7640cfd22
Ahmed <ahmxd989@gmail.com>
2025-05-11 13:15:33 +0400

    updated

--- app/frontend/src/backend/src/entities/user.rs
+++ app/frontend/src/backend/src/entities/user.rs
@@ -5,7 +5,6 @@ use sea_orm::entity::prelude::*;
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: Uuid,
-    pub username: String,
     #[sea_orm(unique)]
     pub email: String,
     pub password_hash: String,
