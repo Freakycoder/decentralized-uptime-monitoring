6ec2a0731d4d5ef3793871c6b195ae7f7b79c09a
Ahmed <ahmxd989@gmail.com>
2025-05-12 22:37:37 +0400

    updated

--- app/backend/migration/src/lib.rs
+++ app/backend/migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 // Import migration modules as they're created
 // The first one will be created by the CLI
 mod m20250511_000001_create_tables;
+mod m20250511_132813_create_tables;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20250511_000001_create_tables::Migration),
+            Box::new(m20250511_132813_create_tables::Migration),
         ]
     }
 }
