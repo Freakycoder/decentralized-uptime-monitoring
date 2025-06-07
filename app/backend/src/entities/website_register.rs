231fda9a9035b2bc4bb0d5ddecbfb3dc555bd041
Ahmed <ahmxd989@gmail.com>
2025-06-07 17:37:33 +0400

    updated

--- app/backend/src/entities/website_register.rs
+++ app/backend/src/entities/website_register.rs
@@ -13,14 +13,6 @@ pub struct Model{
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {
-    #[sea_orm(belongs_to = "super::user::Entity", from = "Column::UserId", to = "super::user::Column::Id")]
-    Users,
 }
 
 impl ActiveModelBehavior for ActiveModel {}
-
-impl Related<super::user::Entity> for Entity {
-    fn to() -> RelationDef {
-        Relation::Users.def()
-    }
-}
