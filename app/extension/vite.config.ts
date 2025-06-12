2bde78e81851ea9c96c5415471b7ed087d95bf8a
Ahmed <ahmxd989@gmail.com>
2025-06-13 00:12:02 +0400

    updated

--- app/extension/vite.config.ts
+++ app/extension/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         background: resolve(__dirname, 'src/background.ts'),
         popup: resolve(__dirname, 'src/popup/popup.html'), 
+        content : resolve(__dirname, 'src/content.ts') 
       },
       output: {
         entryFileNames: '[name].js',
