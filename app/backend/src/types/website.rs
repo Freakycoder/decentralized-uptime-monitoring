da5a08d42e75e69a225064cfd4c3d1dd9ab831d6
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-06-15 23:50:42 +0400

    updated

--- app/backend/src/types/website.rs
+++ app/backend/src/types/website.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AddWebsiteInput{
+    pub user_id: Uuid,
     pub url_to_monitor : String
 }
 
