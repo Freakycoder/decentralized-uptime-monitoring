fb1fc46e28180ba0628f1d2a99b57e7f17f9c2d0
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-06-15 23:48:50 +0400

    updated

--- app/backend/migration/src/lib.rs
+++ app/backend/migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 mod m20250511_132813_create_tables;
 mod m20250608_091629_fix_website_register;
+mod m20250615_174310_added_id;
 
 pub struct Migrator;
 
@@ -10,6 +11,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20250511_132813_create_tables::Migration),
             Box::new(m20250608_091629_fix_website_register::Migration),
+            Box::new(m20250615_174310_added_id::Migration),
         ]
     }
 }
