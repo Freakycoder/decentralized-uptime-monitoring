b316790e3c762ae53a2cd6f981970ca8fc50d326
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-06-19 01:37:32 +0400

    updated

---
+++ app/shared/output/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSocket = exports.sendMessage = exports.initSocket = void 0;
+var websocket_1 = require("./websocket");
+Object.defineProperty(exports, "initSocket", { enumerable: true, get: function () { return websocket_1.initSocket; } });
+Object.defineProperty(exports, "sendMessage", { enumerable: true, get: function () { return websocket_1.sendMessage; } });
+Object.defineProperty(exports, "getSocket", { enumerable: true, get: function () { return websocket_1.getSocket; } });
