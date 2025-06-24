cbaeb7d335176e18a08423cdba9b6cd32f2eb86b
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-06-24 13:51:19 +0400

    updated

---
+++ app/backend/src/types/performance_data.rs
@@ -0,0 +1,23 @@
+use sea_orm::prelude::DateTimeWithTimeZone;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PerfomanceDataInput{
+    pub validator_id : Uuid,
+    pub website_id: Uuid,
+    pub timestamp: DateTimeWithTimeZone,
+    pub http_status_code: Option<f64>,
+    pub dns_resolution_ms: Option<f64>,
+    pub connection_time_ms: Option<f64>,
+    pub tls_handshake_ms: Option<f64>,
+    pub time_to_first_byte_ms: Option<f64>,
+    pub content_download_ms: Option<f64>,
+    pub total_time_ms: Option<f64>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PerformanceOutput{
+    pub status_code : u32,
+    pub message : String
+}
