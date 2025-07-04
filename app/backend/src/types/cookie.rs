9171a99b76a2e5974bb589bb019fbae0a32f84c3
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-07-04 18:38:38 +0400

    updated

--- app/backend/src/types/cookie.rs
+++ app/backend/src/types/cookie.rs
@@ -1,8 +1,9 @@
 use crate::cookie::manager::SessionStore;
-
+use crate::websocket::manager::WebSocketManager;
 // App state that includes both database and session store
 #[derive(Clone)]
 pub struct CookieAppState {
     pub db: sea_orm::DatabaseConnection,
     pub session_store: SessionStore,
+    pub ws : WebSocketManager
 }
