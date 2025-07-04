9e7447ecc51a487af9c4c2beb2cf657f76f6d0d5
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-07-04 20:19:04 +0400

    updated

--- app/backend/src/types/cookie.rs
+++ app/backend/src/types/cookie.rs
@@ -1,9 +1,11 @@
+use std::sync::Arc;
+
 use crate::cookie::manager::SessionStore;
 use crate::websocket::manager::WebSocketManager;
 // App state that includes both database and session store
 #[derive(Clone)]
 pub struct CookieAppState {
     pub db: sea_orm::DatabaseConnection,
-    pub session_store: SessionStore,
-    pub ws : WebSocketManager
+    pub session_store: Arc<SessionStore>,
+    pub ws : Arc<WebSocketManager>
 }
