9a9a3fbe984a16578bc0bde502140ca0e240b5ed
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-07-06 15:24:55 +0400

    updated

--- app/backend/src/types/user.rs
+++ app/backend/src/types/user.rs
@@ -2,58 +2,66 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct UserInput{
-    pub email : String,
-    pub password : String
+pub struct UserInput {
+    pub email: String,
+    pub password: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct SignUpResponse{
-    pub status_code : u32,
-    pub message : String,
-    pub user_id : Option<String>
+pub struct SignUpResponse {
+    pub status_code: u32,
+    pub message: String,
+    pub user_id: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct LoginResponse{
-    pub status_code : u32,
-    pub message : String,
-    pub user_data : Option<UserData>
+pub struct LoginResponse {
+    pub status_code: u32,
+    pub message: String,
+    pub user_data: Option<UserData>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 
-pub struct UserData{
-    pub user_id  : Uuid,
-    pub validator_id : Option<Uuid>
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ValidatorInput{
+pub struct UserData {
     pub user_id: Uuid,
-    pub wallet_address : String,
+    pub validator_id: Option<Uuid>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ValidatorInput {
+    pub user_id: Uuid,
+    pub wallet_address: String,
     pub latitude: f64,
     pub longitude: f64,
-    pub device_id : String
+    pub device_id: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct VerifyValidatorResponse{
-    pub status_code : u32,
-    pub message : String,
-    pub validator_data : Option<ValidatorData>
+pub struct VerifyValidatorResponse {
+    pub status_code: u32,
+    pub message: String,
+    pub validator_data: Option<ValidatorData>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ValidatorData{
-   pub validator_id : Uuid,
-   pub latitude : f64,
-   pub longitude : f64
+pub struct ValidatorData {
+    pub validator_id: Uuid,
+    pub latitude: f64,
+    pub longitude: f64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct VerifySignatureRequest{
-    pub message : String,
-    pub signature : String,
-    pub public_key : String
+pub struct VerifySignatureRequest {
+    pub message: String,
+    pub signature: String,
+    pub public_key: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SessionStatusResponse {
+    pub status_code: u32,
+    pub is_valid: bool,
+    pub user_id: Option<String>,
+    pub validator_id: Option<String>,
 }
