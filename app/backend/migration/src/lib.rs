4ae0cc36f47ad257ddad08708be68aa0d280b4e6
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-07-08 20:41:35 +0400

    updated

--- app/backend/migration/src/lib.rs
+++ app/backend/migration/src/lib.rs
@@ -1,8 +1,7 @@
 pub use sea_orm_migration::prelude::*;
-
-// Only include the migrations we actually want to keep
 mod m20250511_132813_create_tables;
 mod m20250628_090238_notification_table;
+mod m20250708_162547_add_coloms_for_notification; 
 
 pub struct Migrator;
 
@@ -14,6 +13,8 @@ impl MigratorTrait for Migrator {
             Box::new(m20250511_132813_create_tables::Migration),
             // Second migration: adds the Notifications table with proper foreign keys
             Box::new(m20250628_090238_notification_table::Migration),
+            // Third migration: adds website_url and website_id to Notifications table
+            Box::new(m20250708_162547_add_coloms_for_notification::Migration), // ✅ Add here
         ]
     }
 }
